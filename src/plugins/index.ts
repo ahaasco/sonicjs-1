03b125bc9518cc8c567aa67d6470f831706ea5ed
Andrew Haas <ahaas@s-5.com>
2026-01-30 10:33:34 -0700

    feat(01-02): export redirect plugin from plugins index
    - Add redirectManagementPlugin export to src/plugins/index.ts
    - Enables plugin discovery by framework and other components

---
+++ src/plugins/index.ts
@@ -0,0 +1,10 @@
+/**
+ * User Plugins Index
+ * 
+ * Export your custom plugins here.
+ * They will be automatically discovered and loaded by SonicJS.
+ */
+
+export { default as contactFormPlugin } from './contact-form/index'
+export { default as redirectManagementPlugin } from './redirect-management/index'
+
